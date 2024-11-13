4f5186b5810ba66eac354241be4b88e2c85bb2a7
Raylan Araujo <raylanra@gmail.com>
2024-11-13 17:51:44 -0300

    Crias as funções dos botões e listar os passos a serem executados

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// let quantidade = parseInt(document.getElementById('quantidade').value);
+
+// function adicionar() {
+//     console.log(quantidade);
+// }
+
+// function adicionar() {
+//     let quantidadeElement = document.getElementById('quantidade');
+//     let quantidadeString = quantidadeElement.value;
+  
+//     if (quantidadeString === '') {
+//       console.error('O campo quantidade está vazio.');
+//     } else {
+//       let quantidade = parseInt(quantidadeString);
+//       if (isNaN(quantidade)) {
+//         console.error('O valor informado não é um número válido.');
+//       } else if (quantidade == 0){
+//         alert('É preciso inserir uma quantidade válida');
+//       } else {
+//         console.log(`A quantidade escolhida foi: ${quantidade}`);
+//       }
+//     }
+//   }
+
+function adicionar() {
+//recuperar valores: nome do produto; quantidade; valor
+// calcular o preço subtotal por produto
+//atualizar o total-
+
+}
+
+
+function limpar() {
+    
+}
