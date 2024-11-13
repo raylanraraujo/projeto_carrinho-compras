fc7e2ab799a070e5c46b54eae74303eaa99a64a8
Raylan Araujo <raylanra@gmail.com>
2024-11-13 19:26:53 -0300

    Adicionar produtos ao carrinho

--- js/app.js
+++ js/app.js
@@ -22,28 +22,33 @@
 //     }
 //   }
 
-
+let total = 0;  
 function adicionar() {
 //recuperar valores: nome do produto; quantidade; valor
 
 let produto = document.getElementById('produto').value; //usamos o value, para obter o valor escolhido
-let quantidade = document.getElementById('quantidade');
+let quantidade = document.getElementById('quantidade').value;
 let nomeProduto = produto.split('-')[0]; //recurso split separa strings em arrays. Separador nesse caso é o hífen
 let valorUnit = produto.split('R$')[1]; 
 
+/*
 alert(nomeProduto);
 alert(valorUnit);
 alert(produto); 
 alert(quantidade.value); //usamos o value, para obter o valor escolhido
-
+*/
   
 // calcular o preço subtotal por produto
-let subTotal = valorUnit * quantidade.value;
-console.log(subTotal)
+let subTotal = valorUnit * quantidade;
 
-//atualizar o total
-let total = 0;  
-total+=subTotal
+//adicionar ao carrinho
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML += `<section class="carrinho__produtos__produto"> <span class="texto-azul"> ${quantidade}x </span> ${nomeProduto} <span class="texto-azul">R$ ${valorUnit} </span> </section>`;
+
+
+
+//atualizar o total 
+total+=subTotal;
 console.log(total)
 }
 
